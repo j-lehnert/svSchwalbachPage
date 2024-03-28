0bd4f20fcda6b318fe2b7b85f75bd47b7b062928
j-lehnert <lehnert.janick@gmail.com>
2024-03-28 04:03:57 +0100

    added the fetch script to include dates

--- httpdocs/assets/js/fetch.js
+++ httpdocs/assets/js/fetch.js
@@ -1,29 +1,29 @@
 function displayRSSFeed() {
-    fetch('https://schachverein-schwalbach.de/httpdocs/index.php/feed/')
-      .then(response => response.text())
-      .then(data => {
-        const parser = new DOMParser();
-        const xml = parser.parseFromString(data, 'application/xml');
-        const items = xml.querySelectorAll('item');
-        const feedContainer = document.getElementById('rss-feed');
+  fetch('https://schachverein-schwalbach.de/httpdocs/index.php/feed/')
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, 'application/xml');
+      const items = xml.querySelectorAll('item');
+      const feedContainer = document.getElementById('rss-feed');
 
-        let html = '<h2>Aktuellste Blog-Einträge</h2>';
-        html += '<ul>';
-        items.forEach(item => {
-          const title = item.querySelector('title').textContent;
-          const link = item.querySelector('link').textContent;
-          const description = item.querySelector('description').textContent;
+      let html = '<h2>Aktuellste Blog-Einträge</h2>';
+      html += '<ul>';
+      items.forEach(item => {
+        const title = item.querySelector('title').textContent;
+        const link = item.querySelector('link').textContent;
+        const description = item.querySelector('description').textContent;
+        const pubDate = new Date(item.querySelector('pubDate').textContent).toLocaleDateString('de-DE', { year: 'numeric', month: 'long', day: 'numeric' });
 
-          html += `<li><a href="${link}"><h3>${title}</h3></a><p>${description}</p></li>`;
-        });
-        html += '</ul>';
-
-        feedContainer.innerHTML = html;
-      })
-      .catch(error => {
-        console.error('Error fetching RSS feed:', error);
+        html += `<li><a href="${link}"><h3>${title}</h3></a><p>${description}</p><p>Veröffentlicht am: ${pubDate}</p></li>`;
       });
-  }
+      html += '</ul>';
 
-  // Call the function to display the RSS feed
-  displayRSSFeed();
+      feedContainer.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error fetching RSS feed:', error);
+    });
+}
+
+displayRSSFeed();
